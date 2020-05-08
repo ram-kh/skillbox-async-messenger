8bf2a4b2d8da1b187f8bae3d77a957d0d2ab54bf
ram-kh <ram_kh@mail.ru>
2020-05-08 15:07:19 +0500

    First upload to Git
    Signed-off-by: ram-kh <ram_kh@mail.ru>

---
+++ app/server.py
@@ -0,0 +1,105 @@
+"""
+Серверное приложение для соединений
+"""
+import asyncio
+from asyncio import transports
+
+
+class ClientProtocol(asyncio.Protocol):
+    login: str
+    server: 'Server'
+    transport: transports.Transport
+
+    def __init__(self, server: 'Server'):
+        self.server = server
+        self.login = None
+
+    def data_received(self, data: bytes):
+        decoded = data.decode()
+        print(decoded)
+
+        if self.login is None:
+            # login:User
+            login_exist = False
+            if decoded.startswith("login:"):
+                login = decoded.replace("login:", "").replace("\r\n", "")
+                for client in self.server.clients:
+                    if client.login == login:
+                        login_exist = True
+                        break
+                if login_exist:
+                    self.transport.write(f"Логин {login} занят, попробуйте другой".encode())
+                    self.connection_lost()
+                else:
+                    self.login = login
+                    self.transport.write(
+                         f"Привет, {self.login}!".encode()
+                         )
+                    self.send_history()
+                    self.send_message(f"Пользователь {self.login} зашел в чат")
+        else:
+            self.send_message(decoded)
+
+    def send_message(self, message):
+        format_string = f"<{self.login}> {message}"
+        self.server.messages.append(format_string)
+        encoded = format_string.encode()
+        for client in self.server.clients:
+            if client.login != self.login:
+                client.transport.write(encoded)
+
+    def send_history(self):
+        if len(self.server.messages) == 0:
+            self.transport.write(
+                f"Сообщений в чате нет!".encode()
+            )
+        else:
+            if len(self.server.messages) <= 10:
+                n = 1-len(self.server.messages)
+            else:
+                n = -9
+            for i in range(n,0):
+                self.transport.write(
+                    f"{self.server.messages[i]}".encode()
+                )
+
+    def connection_made(self, transport: transports.Transport):
+        self.transport = transport
+        self.server.clients.append(self)
+        print("Соединение установлено")
+
+    def connection_lost(self, exception):
+        self.server.clients.remove(self)
+        print("Соединение разорвано")
+
+
+class Server:
+    clients: list
+    messages: list
+
+    def __init__(self):
+        self.clients = []
+        self.messages = []
+
+    def create_protocol(self):
+        return ClientProtocol(self)
+
+    async def start(self):
+        loop = asyncio.get_running_loop()
+
+        coroutine = await loop.create_server(
+            self.create_protocol,
+            "127.0.0.1",
+            8888
+        )
+
+        print("Сервер запущен ...")
+
+        await coroutine.serve_forever()
+
+
+process = Server()
+try:
+    asyncio.run(process.start())
+except KeyboardInterrupt:
+    print("Сервер остановлен вручную")
